1533946e1cad8bf814d501f2f373c6bf3ac6f928
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2025-10-16 20:27:27 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ export default function App() {
   return (
     <>
       <div className="box">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>lil</div>
+        <div>commit</div>
+        <div>type</div>
+        <div>thing</div>
+        <div>ya</div>
+        <div>know?</div>
        </div>
     </>
   )
