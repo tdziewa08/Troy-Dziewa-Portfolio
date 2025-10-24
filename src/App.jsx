fac0e1a32af84bbcc00637c671eb0a37ed2e7866
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2025-10-24 14:26:04 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     <>
       <Home />
       <Projects />
-      <About />
+      {/* <About /> */}
       <Contact />
     </>
   )
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,5 +1,24 @@
-export default function Contact() {
+import gitImgWhite from "/src/assets/github-mark-white.png"
+
+export default function Contact(props) {
     return (
-        <div id="contact" className="CHANGETHIS">Contact</div>
+        <div id="contact" className="contact-container">
+            <section className="contact-text">
+                <p>
+                    If you would like to reach out to me, view my resume, or explore the rest of my portfolio, please click the links to the right.
+                </p>
+            </section>
+            <section className="contact-links-container">
+                <a className="contact-link" target="_blank" rel="noopener noreferrer">
+                    <img src={gitImgWhite} /> Email
+                </a>
+                <a className="contact-link" target="_blank" rel="noopener noreferrer">
+                    <img src={gitImgWhite} /> GitHub
+                </a>
+                <a className="contact-link" target="_blank" rel="noopener noreferrer">
+                    <img src={gitImgWhite} /> Resume
+                </a>
+            </section>
+        </div>
     )
 }
