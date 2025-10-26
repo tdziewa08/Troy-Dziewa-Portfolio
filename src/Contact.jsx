55c2868e16386f1a63b16e097e92272558abec6f
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2025-10-26 11:30:14 -0400

    added contact section

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,24 +1,47 @@
-import gitImgWhite from "/src/assets/github-mark-white.png"
+import resume from "/src/assets/Copy TroyD Resume 2025.pdf"
+import { FaGithub } from "react-icons/fa"
+import { GrMailOption } from "react-icons/gr"
+import { GrDocument } from "react-icons/gr"
+import { GrCertificate } from "react-icons/gr"
 
 export default function Contact(props) {
     return (
-        <div id="contact" className="contact-container">
-            <section className="contact-text">
-                <p>
-                    If you would like to reach out to me, view my resume, or explore the rest of my portfolio, please click the links to the right.
-                </p>
-            </section>
-            <section className="contact-links-container">
-                <a className="contact-link" target="_blank" rel="noopener noreferrer">
-                    <img src={gitImgWhite} /> Email
-                </a>
-                <a className="contact-link" target="_blank" rel="noopener noreferrer">
-                    <img src={gitImgWhite} /> GitHub
-                </a>
-                <a className="contact-link" target="_blank" rel="noopener noreferrer">
-                    <img src={gitImgWhite} /> Resume
-                </a>
-            </section>
-        </div>
+        <>
+            <h3>Contact Me</h3>
+            <div id="contact" className="contact-container">
+                <div className="contact-card">
+                    <a href="mailto:tdziewa08@gmail.com" className="icon-container">
+                        <GrMailOption />
+                    </a>
+                    <p className="contact-card-desc">
+                        Email
+                    </p>
+                </div>
+                <div className="contact-card">
+                    <a href={resume} target="_blank" rel="noopener noreferrer" className="icon-container">
+                        <GrDocument />
+                    </a>
+                    <p className="contact-card-desc">
+                        Resume
+                    </p>
+                </div>
+                <div className="contact-card">
+                    <a href="https://scrimba.com/certificate-cert2uNjfKA1Joq9N9TD2SnRb9cJy8WBVtzVfwN" target="_blank" rel="noopener noreferrer" className="icon-container">
+                        <GrCertificate />
+                    </a>
+                    <p className="contact-card-desc">
+                        Course Certificate
+                    </p>
+                </div>
+                <div className="contact-card">
+                    <a href="https://github.com/tdziewa08/" target="_blank" rel="noopener noreferrer" className="icon-container">
+                        <FaGithub />
+                    </a>
+                    <p className="contact-card-desc">
+                        GitHub Profile
+                    </p>
+                </div>
+            </div>
+        </>
     )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,9 @@ export default function Home() {
                 <section>
                     <h1>Troy Dziewa</h1>
                     <p className="title">Web Developer</p>
-                    <p>I have been learning web development for the past 3 years, and am still actively learning today.</p>
+                    <p>
+                        I have been learning web development for the past 3 years, and am still actively learning today.
+                    </p>
                 </section>
             </div>
             <div className="profile-img-container">
