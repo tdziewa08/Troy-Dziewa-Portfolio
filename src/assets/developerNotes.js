9f7d2e326db353694b8aa46d90252a338436a73a
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2025-12-27 12:14:08 -0500

    feature/developer-notes
    adjusted media quieries
    
    added dev-notes section

--- src/assets/developerNotes.js
+++ src/assets/developerNotes.js
@@ -1,7 +1,7 @@
 export const developerNotes = {
-    "video-game-bracket": "This app uses the RAWG API to fetch video game names and images. The user decides the parameters for the tournament bracket and selects the winners from each round until there is one ultimate winner. I am most proud of my use of recursion to cleanly fetch from the API, aswell as my derived state logic for determining the beginning and end of rounds. The biggest hurdle I experienced with this app was getting it deployed properly, which got me exposed to Copilot for the first time.",
-    "recipe-finder": "",
-    "the-wisenbergs": "",
-    "quizzical": "",
-    "gold-digger": ""
+    "video-game-bracket": "The main challenge for this app was getting it to deploy safely and correctly. I am most proud of my recursive fetching aswell as my use of derived state for the logic used to start and end rounds.",
+    "recipe-finder": "Reworking this code to bring context values up to the Layout component was the biggest challenge I faced when building this app. However in the end I believe that is what made the app work smoothly and consistently.",
+    "the-wisenbergs": "The placeholder images and lack of real testimonials admittedly make the site feel a little flat at the moment. When given the real personal touches, this website will be a great resource for our trivia team.",
+    "quizzical": "Changing the state of the buttons to display them as blue, green, or red was a real struggle that took longer than expected to fix. Overall I believe the end result is clean, responsive, and enjoyable.",
+    "gold-digger": "My first app built using Node.js. Not very exciting but did expose me to Node and backend development."
 }
